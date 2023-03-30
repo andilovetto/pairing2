315eb2d4731611b963d6ffc65f7f08aeb0c127d3
andi lovetto <andilovetto@gmail.com>
2023-03-29 18:36:57 -0600

    Add an elsif statement

--- plants.rb
+++ plants.rb
@@ -2,6 +2,8 @@ gets_sunshine = false
 ounces_of_water = 10
 if gets_sunshine == true && ounces_of_water >= 10
     puts "This plant will thrive!"
+elsif gets_sunshine == true || ounces_of_water >= 10
+    puts "This plant needs more than that!!"
 else
     puts "This plant will die!"
 end
